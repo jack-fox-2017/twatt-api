0d0e9faf23ebe4aa2298c4ca0989a896fc7749ac
ahmadaidil <ahmdaidil@gmail.com>
2017-08-23 15:09:47 +0700

    search feature done

--- controllers/twatt.js
+++ controllers/twatt.js
@@ -4,7 +4,7 @@ var getTL = (req, res) =>{
   let endpoint = 'https://api.twitter.com/1.1/statuses/home_timeline.json';
   twatt.Get(endpoint, (err, result)=>{
     if(err) res.send(err);
-    else res.json(result);
+    else res.json(JSON.parse(result));
   })
 }
 
@@ -12,7 +12,7 @@ var getUserTL = (req, res) =>{
   let endpoint = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
   twatt.Get(endpoint, (err, result)=>{
     if(err) res.send(err);
-    else res.json(result);
+    else res.json(JSON.parse(result));
   })
 }
 
@@ -20,12 +20,22 @@ var postStatus = (req, res) =>{
   let endpoint = 'https://api.twitter.com/1.1/statuses/update.json';
   twatt.Post(endpoint, req.body.status, (err, result)=>{
     if(err) res.send(err);
-    else res.json(result);
+    else res.json(JSON.parse(result));
+  })
+}
+
+var findByKeyword = (req, res) =>{
+  let keyword = req.params.keyword;
+  let endpoint = `https://api.twitter.com/1.1/search/tweets.json?q=${keyword}&result_type=recent`;
+  twatt.Get(endpoint, (err, result)=>{
+    if(err) res.send(err);
+    else res.json(JSON.parse(result));
   })
 }
 
 module.exports = {
   getTL,
   getUserTL,
-  postStatus
+  postStatus,
+  findByKeyword
 }
--- routes/twatt.js
+++ routes/twatt.js
@@ -4,6 +4,7 @@ const twattController = require('../controllers/twatt');
 
 router.get('/timeline', twattController.getTL);
 router.get('/usertimeline', twattController.getUserTL);
+router.get('/search/:keyword', twattController.findByKeyword);
 router.post('/postStatus', twattController.postStatus);
 
 module.exports = router;
