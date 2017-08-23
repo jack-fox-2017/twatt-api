f681c2ad07aae0039a6e4bee811eb25a81c9c188
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-08-23 17:54:18 +0700

    Timeline Feature, Search Feature, Post Featur sudah selesai

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -26,4 +26,53 @@ var getDataTwit = (req, res) => {
       })
 }
 
-module.exports = getDataTwit;
+var postTwit = (req, res) => {
+  oauth.post(
+  "https://api.twitter.com/1.1/statuses/update.json",
+  process.env.ACCESS_TOKEN,
+  process.env.ACCESS_SECRET,
+  {"status":req.body.text},
+  (err, data) => {
+    if (!err) {
+      res.send(data)
+    } else {
+      res.send(err)
+    }
+  });
+}
+
+var searchTweet = (req, res) => {
+  oauth.get(
+    'https://api.twitter.com/1.1/search/tweets.json?q=hacktiv8',
+    process.env.ACCESS_TOKEN, //test user token
+    process.env.ACCESS_SECRET, //test user secret
+    (err, data) => {
+      if (!err) {
+        res.send(data)
+      } else {
+        res.send(err)
+      }
+    })
+}
+
+var timeLine = (req, res) => {
+  oauth.get(
+    'https://api.twitter.com/1.1/statuses/home_timeline.json',
+    process.env.ACCESS_TOKEN,
+    process.env.ACCESS_SECRET,
+    (err, data) => {
+      if (!err) {
+        res.send(data)
+      } else {
+        res.send(err)
+      }
+    }
+  )
+}
+
+module.exports = {
+  getDataTwit,
+  searchTweet,
+  postTwit,
+  timeLine
+};
--- routes/oauth.js
+++ routes/oauth.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var oauth = require('../controllers/oauth')
 
 /* GET users listing. */
-router.get('/', oauth)
+router.get('/', oauth.getDataTwit)
+router.get('/search', oauth.searchTweet)
+router.post('/post', oauth.postTwit)
+router.get('/timeline', oauth.timeLine)
 
 module.exports = router;
